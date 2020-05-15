ccacd3201008d2f4e5f7132fc36ad8d6c9fc96d8
Mikhailov Sergey <sergeypolet@yandex.ru>
2020-05-15 10:45:05 +0300

    Add test project

---
+++ TestTravis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTravis
+//
+//  Created by Sergey Mikhailov on 15.05.2020.
+//  Copyright © 2020 Sergey Mikhailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
